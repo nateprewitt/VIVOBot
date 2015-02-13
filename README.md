2b2a17709d867e3e2c4dca11d20a9173a970c6ed
Nate Prewitt <Nate.Prewitt@colorado.edu>
2015-02-13 16:19:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+VIVOBot
+--------------
+
+VIVOBot is a simple python class used to perform basic actions in VIVO via a scripting interface.
+
+This will allow you to do things like run inference, upload an ontology file, or perform queries with a cronjob.
+
+You can also use this to perform quick data retrievals out of VIVO for data mashups or complex processing that you
+would prefer to do in a programming environment.
+
---
+++ vivo_bot.py
@@ -0,0 +1,116 @@
+import requests
+import json
+import sys
+import urllib
+import logging
+
+class VIVOBot(object):
+
+    """ VIVOBot is a simple class to interact with various functionality in
+        the vivo siteAdmin console via http requests.
+    """
+    def __init__(self, filename="vivobot.cfg", debug=2):
+
+        self.configfile = filename
+        self.set_debug(debug)
+        vardict = self.ingest_config()
+        self.server = vardict.get('server')
+        self.cookies = self.login(vardict.get('uname'), vardict.get('pass'))
+        
+    def ingest_config(self):
+
+        """ Opens configuration file and loads contents as JSON """
+        try:
+            with open(self.configfile, 'rb') as f:
+                return json.loads(f.read())
+        except IOError:
+            logging.critical("No config file was found, please ensure path " 
+                             "is correct.")
+            sys.exit(1)
+        except ValueError:
+            logging.critical("The config file %s is imporperly configured. "
+                            "Please ensure it adheres to "
+                            "the JSON format.") % self.configfile
+            sys.exit(1)
+
+    def set_debug(self, debug):
+
+        """ Convert numerical debugging level to logging variable """
+        if debug < 1 :
+            logging.basicConfig(level=logging.CRITICAL)
+        elif debug == 1:
+            logging.basicConfig(level=logging.ERROR)
+        elif debug == 2:
+            logging.basicConfig(level=logging.WARNING)
+        elif debug == 3:
+            logging.basicConfig(level=logging.INFO)
+        else:
+            logging.basicConfig(level=logging.DEBUG)
+
+    def login(self, uname, passw):
+
+        """ Perform login to VIVO and set cookie for transactions """
+        data = {'loginName': uname,
+                'loginPassword': passw,
+                'loginForm': 'Log in'}
+
+        r1 = requests.get(self.server+"/login")
+        c1 = r1.cookies
+        r = requests.post(self.server+"/authenticate", data=data, cookies=c1)
+        #!!! Possibly handle HTTP 30x redirects here (http v https issue)
+        return r.cookies
+
+    def query_triplestore(self, query, prefixes=None):
+
+        """ Perform SPARQL Query against the VIVO SPARQL console """
+        if not prefixes:
+            try:
+                with open('prefixes.txt', 'r') as f:
+                    prefixes = f.read()
+            except IOError:
+                logging.warning("Unable to find prefixes file and no prefixes "
+                               "were provided. Running query without prefixes.")
+                prefixes = ""
+
+        data = {"query": prefixes+'\n'+query,
+                "resultFormat": "application/sparql-results+json",
+                "rdfResultFormat": "text/turtle"}
+
+        url = "/admin/sparqlquery?"+urllib.urlencode(data)
+        r = requests.get(self.server+url, cookies=self.cookies)
+        return r.content
+
+    def rebuild_search_index(self):
+
+        """ Trigger a reload of the search index """
+        r = requests.post(self.server+"/SearchIndex")
+        # Possibly try to do a check with status bar until it's done?
+
+    def recompute_inference(self):
+
+        """ Trigger the system to start reinferencing """
+        r = requests.post(self.server+"/RecomputeInferences") 
+
+    def upload_file(self, loc, rdftype="file", lang="N3", mode="add", cg=True):
+        
+        """ Upload an ontology file into VIVO """
+        if lang not in ('RDF/XML', 'N3', 'N-TRIPLE', 'TTL'):
+            logging.warning("Unrecognized Language Parameter for "
+                            "resource request %s") % loc
+        if mode not in ('directAddABox', 'add', 'remove'):
+            logging.warning("Unrecognized Mode Parameter for "
+                           "resource request %s") % loc 
+        data = {"mode": mode,
+                "language": lang,
+                "makeClassgroups": cg}
+        if rdftype.lower() == "file":
+            data['rdfStream'] = loc
+        elif rdftype.lower() == "url":
+            data['rdfUrl'] = loc
+        else:
+            logging.warning("Unrecognized Location Parameter for "
+                           "resource request %s") % loc
+
+        headers = {"Content-Type": "multipart/form-data"}
+        r = requests.post(self.server+"/uploadRDF", data=filename, headers=headers)
+        #Return status code?
