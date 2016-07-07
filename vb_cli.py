15ef7dfb73bafe003cad5b41ac0322a3ddd29395
Nate Prewitt <Nate.Prewitt@gmail.com>
2016-07-06 20:44:53 -0600

    docstring cleanup

--- vb_cli.py
+++ vb_cli.py
@@ -4,14 +4,14 @@ import inspect
 
 from vivo_bot import VIVOBot
 
-""" This is a command line interface for VIVOBot to allow people wishing to
-    avoid writing Python code to run small 'task' files in cronjobs or for
-    simple report generation
+"""This is a command line interface for VIVOBot to allow people wishing to
+avoid writing Python code to run small 'task' files in cronjobs or for
+simple report generation.
 """
 
-def run(vb, task_list_file):
 
-    """ Open task file and run commands listed """
+def run(vb, task_list_file):
+    """Open task file and run commands listed"""
     with open(task_list_file, 'r') as f:
         for n, task in enumerate(f):
             if task.endswith('\n'):
@@ -20,19 +20,19 @@ def run(vb, task_list_file):
             result = process_command(vb, cmd, args)
             log_result(result, n)
 
-def process_args(task):
 
-    """ Extract command from line and parse arguments """
+def process_args(task):
+    """Extract command from line and parse arguments"""
     task_parts = task.split(' ')
     cmd = task_parts[0]
     args = []
     if len(task_parts) > 1:
         args = split_args(task_parts[1:])
     return cmd, args
-        
-def split_args(task_parts):
 
-    """ Split arguments on spaces and check for strings wrapped in quotes """
+
+def split_args(task_parts):
+    """Split arguments on spaces and check for strings wrapped in quotes"""
     quoteflag = False
     args = []
     for n, part in enumerate(task_parts):
@@ -52,32 +52,32 @@ def split_args(task_parts):
         args = []
     return args
 
-def process_command(vb, cmd, args):
 
-    """ Retrieve command from VIVOBot Object and execute it """
+def process_command(vb, cmd, args):
+    """Retrieve command from VIVOBot Object and execute it"""
     command = getattr(vb, cmd)
     if not command:
         print "ERROR: Unreconized Command - %s" % cmd
     arg_list = inspect.getargspec(command)[0]
     return command(*args)
 
-def log_result(result, n):
 
-    """ Write response from server to a file """
+def log_result(result, n):
+    """Write response from server to a file"""
     if result:
         t = time.strftime("%d-%m-%YT%H.%M.%S")
         with open("logs/"+t+"_cmd_"+str(n), "w") as f:
             f.write(result)
-    
+
 
 if __name__ == "__main__":
 
-   if len(sys.argv) == 3:
-       vb = VIVOBot(sys.argv[1])
-   elif len(sys.argv) > 3:
-       vb = VIVOBot(sys.argv[1], sys.argv[3])
-   else:
-       print "Usage: python vb_cli.py [config_file] [task_list] [debug_level]"
-       sys.exit(1)
+    if len(sys.argv) == 3:
+        vb = VIVOBot(sys.argv[1])
+    elif len(sys.argv) > 3:
+        vb = VIVOBot(sys.argv[1], sys.argv[3])
+    else:
+        print "Usage: python vb_cli.py [config_file] [task_list] [debug_level]"
+        sys.exit(1)
 
-   run(vb, sys.argv[2])
+    run(vb, sys.argv[2])
--- vivo_bot.py
+++ vivo_bot.py
@@ -1,42 +1,42 @@
-import requests
 import json
 import sys
 import urllib
 import logging
 
-class VIVOBot(object):
+import requests
 
-    """ VIVOBot is a simple class to interact with various functionality in
-        the vivo siteAdmin console via http requests.
+
+class VIVOBot(object):
+    """VIVOBot is a simple class to interact with various functionality in
+    the vivo siteAdmin console via http requests.
     """
+
     def __init__(self, filename="config/vivobot.cfg", debug=2):
 
         self.configfile = filename
         self.set_debug(debug)
         vardict = self.ingest_config()
-        self.server = vardict.get('server',"")
+        self.server = vardict.get('server', "")
         self.cookies = self.login(vardict.get('uname'), vardict.get('pass'))
-        
-    def ingest_config(self):
 
-        """ Opens configuration file and loads contents as JSON """
+    def ingest_config(self):
+        """Opens configuration file and loads contents as JSON"""
         try:
             with open(self.configfile, 'rb') as f:
                 return json.loads(f.read())
         except IOError:
-            logging.critical("No config file was found, please ensure path " 
+            logging.critical("No config file was found, please ensure path "
                              "is correct.")
             sys.exit(1)
         except ValueError:
             logging.critical("The config file %s is imporperly configured. "
-                            "Please ensure it adheres to "
-                            "the JSON format.") % self.configfile
+                             "Please ensure it adheres to "
+                             "the JSON format.") % self.configfile
             sys.exit(1)
 
     def set_debug(self, debug):
-
-        """ Convert numerical debugging level to logging variable """
-        if debug < 1 :
+        """Convert numerical debugging level to logging variable"""
+        if debug < 1:
             logging.basicConfig(level=logging.CRITICAL)
         elif debug == 1:
             logging.basicConfig(level=logging.ERROR)
@@ -48,8 +48,7 @@ class VIVOBot(object):
             logging.basicConfig(level=logging.DEBUG)
 
     def login(self, uname, passw):
-
-        """ Perform login to VIVO and set cookie for transactions """
+        """Perform login to VIVO and set cookie for transactions"""
         data = {'loginName': uname,
                 'loginPassword': passw,
                 'loginForm': 'Log in'}
@@ -57,7 +56,7 @@ class VIVOBot(object):
         r1 = requests.get(self.server+"/login")
         c1 = r1.cookies
         r = requests.post(self.server+"/authenticate", data=data, cookies=c1)
-        #!!! Possibly handle HTTP 30x redirects here (http v https issue)
+        # !!! Possibly handle HTTP 30x redirects here (http v https issue)
         if r.status_code == 200:
             return c1
         else:
@@ -65,15 +64,15 @@ class VIVOBot(object):
             raise EnvironmentError("Unable to authenticate user")
 
     def query_triplestore(self, query, prefixes=None):
-
-        """ Perform SPARQL Query against the VIVO SPARQL console """
+        """Perform SPARQL Query against the VIVO SPARQL console"""
         if not prefixes:
             try:
                 with open('config/prefixes.txt', 'r') as f:
                     prefixes = f.read()
             except IOError:
                 logging.warning("Unable to find prefixes file and no prefixes "
-                               "were provided. Running query without prefixes.")
+                                "were provided. Running query without "
+                                "prefixes.")
                 prefixes = ""
 
         data = {"query": prefixes+'\n'+query,
@@ -85,26 +84,23 @@ class VIVOBot(object):
         return r.content
 
     def rebuild_search_index(self):
-
-        """ Trigger a reload of the search index """
+        """Trigger a reload of the search index"""
         data = {'rebuild': 'Rebuild'}
         r = requests.post(self.server+"/SearchIndex", data=data, cookies=self.cookies)
         # Possibly try to do a check with status bar until it's done?
 
     def recompute_inference(self):
-
-        """ Trigger the system to start reinferencing """
-        r = requests.post(self.server+"/RecomputeInferences") 
+        """Trigger the system to start reinferencing"""
+        r = requests.post(self.server+"/RecomputeInferences")
 
     def upload_file(self, loc, rdftype="file", lang="N3", mode="add", cg=True):
-        
-        """ Upload an ontology file into VIVO """
+        """Upload an ontology file into VIVO"""
         if lang not in ('RDF/XML', 'N3', 'N-TRIPLE', 'TTL'):
             logging.warning("Unrecognized Language Parameter for "
                             "resource request %s") % loc
         if mode not in ('directAddABox', 'add', 'remove'):
             logging.warning("Unrecognized Mode Parameter for "
-                           "resource request %s") % loc 
+                            "resource request %s") % loc
         data = {"mode": mode,
                 "language": lang,
                 "makeClassgroups": cg}
@@ -114,8 +110,8 @@ class VIVOBot(object):
             data['rdfUrl'] = loc
         else:
             logging.warning("Unrecognized Location Parameter for "
-                           "resource request %s") % loc
+                            "resource request %s") % loc
 
         headers = {"Content-Type": "multipart/form-data"}
-        r = requests.post(self.server+"/uploadRDF", data=filename, headers=headers)
-        #Return status code?
+        r = requests.post(self.server+"/uploadRDF", data=data, headers=headers)
+        # Return status code?
